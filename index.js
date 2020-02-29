108fc940a21ec9826969de902d56193414c28af1
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-02-29 19:36:41 +0200

    create flipCard func

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ const cards = document.querySelectorAll(".memory-card");
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add("flip");
+
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+
+  // second click
+  secondCard = this;
+
+  checkForMatch();
+}
