268eb5bd809f3335b1066c3a87dae1febce1f810
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-09-15 23:36:38 +0300

    realize functional for first click on card

--- memoryGame.js
+++ memoryGame.js
@@ -12,4 +12,14 @@ cards.forEach((card) => card.addEventListener("click", flipCard));
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
+
+  this.classList.add("flip");
+
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
 }
