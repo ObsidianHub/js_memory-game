b624525d23613e104396ab4ce2d1bdfee6c45edb
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-09-15 23:40:58 +0300

    create main variables, fix a little bugs

--- memoryGame.js
+++ memoryGame.js
@@ -1,11 +1,15 @@
+const cards = document.querySelectorAll(".memory-card");
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
 (function shuffle() {
   cards.forEach((card) => {
     let randomPos = Math.floor(Math.random() * 12);
     card.style.order = randomPos;
   });
 })();
-
-const cards = document.querySelectorAll(".memory-card");
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
 
