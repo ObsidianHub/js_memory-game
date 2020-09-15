3cc140d02e073db1aefc41da3aef6dd091f08783
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-09-15 23:35:02 +0300

    create shuffle function as self-called and mix our cards under refresh page

--- memoryGame.js
+++ memoryGame.js
@@ -1,3 +1,8 @@
 const cards = document.querySelectorAll(".memory-card");
 
-cards.forEach((card) => card.addEventListener("click", flipCard));
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
