547311ee73c4c4a345e5dd80ccc5e2be50aadeaf
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-09-15 23:35:40 +0300

    add click listener for cards

--- memoryGame.js
+++ memoryGame.js
@@ -1,8 +1,10 @@
-const cards = document.querySelectorAll(".memory-card");
-
 (function shuffle() {
   cards.forEach((card) => {
     let randomPos = Math.floor(Math.random() * 12);
     card.style.order = randomPos;
   });
 })();
+
+const cards = document.querySelectorAll(".memory-card");
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
